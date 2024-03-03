8c9deaad9bfbc8bea0a66128868466b1c19aa54a
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-03-03 15:21:55 +0200

    adding hangman drawing

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,14 @@ const words=["apple","cherry","grape","lemon"];
 const randomNum = Math.floor(Math.random()*words.length);
 const randomWord = words[randomNum];
 const letters = document.querySelectorAll(".letter");
+const one=head();
+const two=body();
+const three= leftHand();
+const four=rightHand();
+const five =leftLeg();
+const six= rightLeg();
 let clickedLetter;
-
+let hangmanArray = [one,two,three,four,five,six]; 
 hidden=randomWord.replace(/[a-z]/gi , '-')
 
 function displayWord(){
@@ -20,28 +26,26 @@ function clickLetters(){
             letters[i].classList.remove('pressed');
           }, 200);
           console.log(letters[i])
-        if (randomWord[i]==clickedLetter){
-            const  newHidden=hidden.substring(0, i) + clickedLetter + hidden.substring(i + 1);
+          if (randomWord[i]==clickedLetter){
+            const  newHidden=hidden.substring(0, j) + clickedLetter + hidden.substring(j+ 1);
             hidden=newHidden
             document.getElementById("answer-section").innerHTML=hidden
         }
+        else{
+            hangmanArray[Math.floor(Math.random() * hangmanArray.length)]();
+    }
         
         })
-       
-        }
-
-    }  
+        
+    }}
 
 
+    
         
         
 
-
-
-        
-
-        
-        
+    
+    
         
     
 
